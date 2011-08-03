dfc6c8b8b8ed238c9f3e96e3855dd10eed891a0e
Ali Muzaffar <atgheb@gmail.com>
2011-08-03 14:11:28 +0500

    Fixed mistake in tile.xml, added date validation

--- src/main/java/com/groupdealclone/web/NewCampaignController.java
+++ src/main/java/com/groupdealclone/web/NewCampaignController.java
@@ -1,18 +1,21 @@
 package com.groupdealclone.web;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.Map;
 
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.groupdealclone.domain.Campaign;
-import com.groupdealclone.domain.Deal;
 import com.groupdealclone.service.CampaignManager;
 import com.groupdealclone.validation.CampaignValidator;
 
@@ -31,7 +34,7 @@ public class NewCampaignController {
 	}
 
 	@RequestMapping(value = "new-campaign-input", method = RequestMethod.POST)
-	public String processForm(@ModelAttribute("campaign") Campaign campaignForm, BindingResult result, Map<String,Object> model) {
+	public String processForm(@Valid Campaign campaignForm, BindingResult result, Map<String,Object> model) {
 		new CampaignValidator().validate(campaignForm, result);
 		if (result.hasErrors()) {
 			return "new-campaign-input";
@@ -43,6 +46,15 @@ public class NewCampaignController {
 	
 	public void setCampaignManager(CampaignManager dealManager){
 		this.campaignManager = dealManager;
+	}
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+	    SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+	    dateFormat.setLenient(false);
+
+	    // true passed to CustomDateEditor constructor means convert empty String to null
+	    binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
 	}
 
 }
